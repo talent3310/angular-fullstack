efb34a70b8a200867d01be5aee10e30b094b9c42
talent3310 <talent3310@own.com>
2017-06-26 20:22:42 +0200

    sign up page created

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -2,6 +2,7 @@
 // @flow
 
 import angular from 'angular';
+import $ from "jquery";
 
 type User = {
   name: string;
@@ -24,17 +25,64 @@ export default class SignupController {
   constructor(Auth, $state) {
     this.Auth = Auth;
     this.$state = $state;
+    //jquery signup steps
+    var navListItems = $('div.setup-panel div a'),
+      allWells = $('.setup-content'),
+      allNextBtn = $('.nextBtn'),
+      allNotAgreeBtn = $('.notAgreeBtn');
+    allWells.hide();
+
+    navListItems.click(function(e) {
+      e.preventDefault();
+      var $target = $($(this).attr('href')),
+        $item = $(this);
+
+      if (!$item.hasClass('disabled')) {
+        navListItems.removeClass('btn-primary').addClass('btn-default');
+        $item.addClass('btn-primary');
+        allWells.hide();
+        $target.show();
+        $target.find('input:eq(0)').focus();
+      }
+    });
+
+    allNextBtn.click(function() {
+
+      var curStep = $(this).closest(".setup-content"),
+        curStepBtn = curStep.attr("id"),
+        nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+        curInputs = curStep.find("input[type='text'],input[type='url']"),
+        isValid = true;
+
+      $(".form-group").removeClass("has-error");
+      for (var i = 0; i < curInputs.length; i++) {
+        if (!curInputs[i].validity.valid) {
+          isValid = false;
+          $(curInputs[i]).closest(".form-group").addClass("has-error");
+        }
+      }
+
+      if (isValid)
+        nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    allNotAgreeBtn.click(function() {
+      alert('약관에 동의하셔야 회원가입이 가능합니다.');
+    });
+    $('div.setup-panel div a.btn-primary').trigger('click');
+    // end-jquery signup steps
+
   }
 
   register(form) {
     this.submitted = true;
 
-    if(form.$valid) {
+    if (form.$valid) {
       return this.Auth.createUser({
-        name: this.user.name,
-        email: this.user.email,
-        password: this.user.password
-      })
+          name: this.user.name,
+          email: this.user.email,
+          password: this.user.password
+        })
         .then(() => {
           // Account created, redirect to home
           this.$state.go('main');
